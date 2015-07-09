05948b20401c18d47e4930163e50a06dddc2ca1a
jsr166 <jsr166>
2015-07-09 00:58:30 +0000

    fix theoretical race; must recheck count.get() while holding takeLock

--- src/main/java/util/concurrent/LinkedBlockingQueue.java
+++ src/main/java/util/concurrent/LinkedBlockingQueue.java
@@ -478,8 +478,7 @@ public class LinkedBlockingQueue<E> extends AbstractQueue<E>
         final ReentrantLock takeLock = this.takeLock;
         takeLock.lock();
         try {
-            Node<E> first = head.next;
-            return (first == null) ? null : first.item;
+            return (count.get() > 0) ? head.next.item : null;
         } finally {
             takeLock.unlock();
         }
