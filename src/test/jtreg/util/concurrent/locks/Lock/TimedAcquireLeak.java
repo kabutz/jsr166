7d8d413d0df616c54e6597c2e29da35d4d1b50b9
jsr166 <jsr166>
2015-09-04 23:35:50 +0000

    merge upstream changes

--- src/test/jtreg/util/concurrent/locks/Lock/TimedAcquireLeak.java
+++ src/test/jtreg/util/concurrent/locks/Lock/TimedAcquireLeak.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -148,7 +148,7 @@ public class TimedAcquireLeak {
             String.valueOf(new Random().nextInt(Integer.MAX_VALUE));
 
         final String[] jobCmd = {
-            java, "-Xmx8m",
+            java, "-Xmx8m", "-XX:+UsePerfData",
             "-classpath", System.getProperty("test.classes", "."),
             childClassName, uniqueID
         };
