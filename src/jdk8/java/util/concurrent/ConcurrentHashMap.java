179647a2a2c322da794a5cfa07de9a2b2753be5d
jsr166 <jsr166>
2017-03-08 22:13:42 +0000

    8176402: parameter name switcharoo in ConcurrentHashMap

--- src/jdk8/java/util/concurrent/ConcurrentHashMap.java
+++ src/jdk8/java/util/concurrent/ConcurrentHashMap.java
@@ -3432,9 +3432,9 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K,V>
 
     static final class KeyIterator<K,V> extends BaseIterator<K,V>
         implements Iterator<K>, Enumeration<K> {
-        KeyIterator(Node<K,V>[] tab, int index, int size, int limit,
+        KeyIterator(Node<K,V>[] tab, int size, int index, int limit,
                     ConcurrentHashMap<K,V> map) {
-            super(tab, index, size, limit, map);
+            super(tab, size, index, limit, map);
         }
 
         public final K next() {
@@ -3452,9 +3452,9 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K,V>
 
     static final class ValueIterator<K,V> extends BaseIterator<K,V>
         implements Iterator<V>, Enumeration<V> {
-        ValueIterator(Node<K,V>[] tab, int index, int size, int limit,
+        ValueIterator(Node<K,V>[] tab, int size, int index, int limit,
                       ConcurrentHashMap<K,V> map) {
-            super(tab, index, size, limit, map);
+            super(tab, size, index, limit, map);
         }
 
         public final V next() {
@@ -3472,9 +3472,9 @@ public class ConcurrentHashMap<K,V> extends AbstractMap<K,V>
 
     static final class EntryIterator<K,V> extends BaseIterator<K,V>
         implements Iterator<Map.Entry<K,V>> {
-        EntryIterator(Node<K,V>[] tab, int index, int size, int limit,
+        EntryIterator(Node<K,V>[] tab, int size, int index, int limit,
                       ConcurrentHashMap<K,V> map) {
-            super(tab, index, size, limit, map);
+            super(tab, size, index, limit, map);
         }
 
         public final Map.Entry<K,V> next() {
