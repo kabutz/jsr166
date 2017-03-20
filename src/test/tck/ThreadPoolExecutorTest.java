21ef44522ae08778921cb231ec1f2c660ff7f921
jsr166 <jsr166>
2017-03-20 00:21:54 +0000

    testSaturatedSubmitCallable: simplify, fixing errorprone warning: [FutureReturnValueIgnored]

--- src/test/tck/ThreadPoolExecutorTest.java
+++ src/test/tck/ThreadPoolExecutorTest.java
@@ -18,7 +18,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
@@ -1112,7 +1111,7 @@ public class ThreadPoolExecutorTest extends JSR166TestCase {
                     await(done);
                 }};
             for (int i = 0; i < 2; ++i)
-                p.submit(Executors.callable(task));
+                p.execute(task);
             for (int i = 0; i < 2; ++i) {
                 try {
                     p.execute(task);
