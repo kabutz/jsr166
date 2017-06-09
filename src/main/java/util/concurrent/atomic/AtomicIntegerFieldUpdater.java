13122150f8721418280247acfaf737318b01757f
jsr166 <jsr166>
2017-06-09 20:06:26 +0000

    whitespace

--- src/main/java/util/concurrent/atomic/AtomicIntegerFieldUpdater.java
+++ src/main/java/util/concurrent/atomic/AtomicIntegerFieldUpdater.java
@@ -294,8 +294,8 @@ public abstract class AtomicIntegerFieldUpdater<T> {
      * Atomically updates (with memory effects as specified by {@link
      * VarHandle#compareAndSet}) the field of the given object managed
      * by this updater with the results of applying the given function
-     * to the current and given values, returning the previous
-     * value. The function should be side-effect-free, since it may be
+     * to the current and given values, returning the previous value.
+     * The function should be side-effect-free, since it may be
      * re-applied when attempted updates fail due to contention among
      * threads.  The function is applied with the current value as its
      * first argument, and the given update as the second argument.
@@ -320,8 +320,8 @@ public abstract class AtomicIntegerFieldUpdater<T> {
      * Atomically updates (with memory effects as specified by {@link
      * VarHandle#compareAndSet}) the field of the given object managed
      * by this updater with the results of applying the given function
-     * to the current and given values, returning the updated
-     * value. The function should be side-effect-free, since it may be
+     * to the current and given values, returning the updated value.
+     * The function should be side-effect-free, since it may be
      * re-applied when attempted updates fail due to contention among
      * threads.  The function is applied with the current value as its
      * first argument, and the given update as the second argument.
