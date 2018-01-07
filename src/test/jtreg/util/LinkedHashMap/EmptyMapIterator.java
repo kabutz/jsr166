e923ed8d1e484c9a4fac2c41f536ef04e8f82c97
jsr166 <jsr166>
2018-01-07 20:29:15 +0000

    remove self-assignment

--- src/test/jtreg/util/LinkedHashMap/EmptyMapIterator.java
+++ src/test/jtreg/util/LinkedHashMap/EmptyMapIterator.java
@@ -32,7 +32,7 @@ import java.util.*;
 public class EmptyMapIterator {
     public static void main(String[] args) throws Exception {
         HashMap map = new HashMap();
-        Iterator iter = iter = map.entrySet().iterator();
+        Iterator iter = map.entrySet().iterator();
         map.put("key", "value");
 
         try {
