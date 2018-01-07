3e72a7b2611c65fc5ac5ef6a068b8542b89db7eb
jsr166 <jsr166>
2018-01-07 22:18:01 +0000

    s/StringBuffer/StringBuilder/

--- src/test/jtreg/util/concurrent/Exchanger/LoopHelpers.java
+++ src/test/jtreg/util/concurrent/Exchanger/LoopHelpers.java
@@ -68,7 +68,7 @@ class LoopHelpers {
         String num = Long.toString(n);
         if (num.length() >= field.length())
             return num;
-        StringBuffer b = new StringBuffer(field);
+        StringBuilder b = new StringBuilder(field);
         b.replace(b.length()-num.length(), b.length(), num);
         return b.toString();
     }
