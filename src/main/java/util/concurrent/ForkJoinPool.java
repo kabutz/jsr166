6beb6aaf81e311cda2993510e09c9b37b9f731ce
jsr166 <jsr166>
2018-01-17 05:53:23 +0000

    typo

--- src/main/java/util/concurrent/ForkJoinPool.java
+++ src/main/java/util/concurrent/ForkJoinPool.java
@@ -718,7 +718,7 @@ public class ForkJoinPool extends AbstractExecutorService {
      * Maximum capacity for queue arrays. Must be a power of two less
      * than or equal to 1 << (31 - width of array entry) to ensure
      * lack of wraparound of index calculations, but defined to a
-     * value a bit less than this to help users trap runway programs
+     * value a bit less than this to help users trap runaway programs
      * before saturating systems.
      */
     static final int MAXIMUM_QUEUE_CAPACITY = 1 << 26; // 64M
