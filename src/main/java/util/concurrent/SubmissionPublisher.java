123bd0d8b11a0e4904e5dda11007afb11ab82439
jsr166 <jsr166>
2018-02-10 20:59:32 +0000

    rename growAndoffer to growAndOffer

--- src/main/java/util/concurrent/SubmissionPublisher.java
+++ src/main/java/util/concurrent/SubmissionPublisher.java
@@ -1067,7 +1067,7 @@ public class SubmissionPublisher<T> implements Publisher<T>,
             if (cap > 0) {
                 boolean added;
                 if (n >= cap && cap < maxCapacity) // resize
-                    added = growAndoffer(item, a, t);
+                    added = growAndOffer(item, a, t);
                 else if (n >= cap || unowned)      // need volatile CAS
                     added = QA.compareAndSet(a, i, null, item);
                 else {                             // can use release mode
@@ -1086,7 +1086,7 @@ public class SubmissionPublisher<T> implements Publisher<T>,
          * Tries to expand buffer and add item, returning true on
          * success. Currently fails only if out of memory.
          */
-        final boolean growAndoffer(T item, Object[] a, int t) {
+        final boolean growAndOffer(T item, Object[] a, int t) {
             int cap = 0, newCap = 0;
             Object[] newArray = null;
             if (a != null && (cap = a.length) > 0 && (newCap = cap << 1) > 0) {
