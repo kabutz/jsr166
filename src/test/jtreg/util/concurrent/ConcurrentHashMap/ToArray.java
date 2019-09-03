d851cc89ecd06a1965d333aa0d79dce2f0c1d261
jsr166 <jsr166>
2019-09-03 22:06:42 +0000

    cleanup pass

--- src/test/jtreg/util/concurrent/ConcurrentHashMap/ToArray.java
+++ src/test/jtreg/util/concurrent/ConcurrentHashMap/ToArray.java
@@ -46,9 +46,11 @@ public class ToArray {
 
     static void executeTest() throws Throwable {
         final ConcurrentHashMap<Integer, Integer> m = new ConcurrentHashMap<>();
+        final ThreadLocalRandom rnd = ThreadLocalRandom.current();
         final int nCPU = Runtime.getRuntime().availableProcessors();
-        final int maxWorkers = Math.max(2, Math.min(32, nCPU));
-        final int nWorkers = ThreadLocalRandom.current().nextInt(2, maxWorkers + 1);
+        final int minWorkers = 2;
+        final int maxWorkers = Math.max(minWorkers, Math.min(32, nCPU));
+        final int nWorkers = rnd.nextInt(minWorkers, maxWorkers + 1);
         final int sizePerWorker = 1024;
         final int maxSize = nWorkers * sizePerWorker;
 
@@ -63,25 +65,26 @@ public class ToArray {
 
             private boolean checkProgress(Object[] a) {
                 int size = a.length;
-                if (size < prevSize) throw new AssertionError("WRONG WAY");
-                if (size > maxSize) throw new AssertionError("OVERSHOOT");
+                if (size < prevSize || size > maxSize)
+                    throw new AssertionError(
+                        String.format("prevSize=%d size=%d maxSize=%d",
+                                      prevSize, size, maxSize));
                 prevSize = size;
                 return size == maxSize;
             }
 
             public void run() {
                 Integer[] empty = new Integer[0];
-                boolean done;
-                do {
-                    done = checkProgress(m.values().toArray())
+                for (;;)
+                    if (checkProgress(m.values().toArray())
                         & checkProgress(m.keySet().toArray())
                         & checkProgress(m.values().toArray(empty))
-                        & checkProgress(m.keySet().toArray(empty));
-                } while (!done);
+                        & checkProgress(m.keySet().toArray(empty)))
+                        return;
             }
         });
 
-        // Each worker will put globally unique keys into the map
+        // Each worker puts globally unique keys into the map
         List<CompletableFuture<?>> workers =
             IntStream.range(0, nWorkers)
             .mapToObj(w -> (Runnable) () -> {
