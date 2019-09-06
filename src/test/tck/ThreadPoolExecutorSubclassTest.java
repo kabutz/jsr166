34fcaeb03b6b3aa168403ac3b18aec593658fc3a
jsr166 <jsr166>
2019-09-06 18:43:35 +0000

    use LONGER_DELAY_MS

--- src/test/tck/ThreadPoolExecutorSubclassTest.java
+++ src/test/tck/ThreadPoolExecutorSubclassTest.java
@@ -769,7 +769,7 @@ public class ThreadPoolExecutorSubclassTest extends JSR166TestCase {
         Runnable waiter = new CheckedRunnable() { public void realRun() {
             threadsStarted.countDown();
             try {
-                MILLISECONDS.sleep(2 * LONG_DELAY_MS);
+                MILLISECONDS.sleep(LONGER_DELAY_MS);
             } catch (InterruptedException success) {}
             ran.getAndIncrement();
         }};
