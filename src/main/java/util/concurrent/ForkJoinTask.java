c5cc48e05737a9a63d062d1d391609542aeee464
jsr166 <jsr166>
2020-08-12 15:52:08 +0000

    fix errorprone [UnusedVariable]

--- src/main/java/util/concurrent/ForkJoinTask.java
+++ src/main/java/util/concurrent/ForkJoinTask.java
@@ -812,7 +812,7 @@ public abstract class ForkJoinTask<V> implements Future<V>, Serializable {
             }
         }
         if (ex != null) {
-            for (int i = 1, s; i <= last; ++i)
+            for (int i = 1; i <= last; ++i)
                 cancelIgnoringExceptions(ts.get(i));
             rethrow(ex);
         }
